80f623c86f15c1bdbcdbfbdd1194b9440a3d30b3
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-10-26 18:31:44 -0400

    added rest client

---
+++ app/src/main/java/muntaserahmed/wifisentry/RestClient.java
@@ -0,0 +1,28 @@
+package muntaserahmed.wifisentry;
+
+import com.loopj.android.http.*;
+
+/**
+ * Created by muntaserahmed on 10/26/14.
+ */
+public class RestClient {
+
+    private static final String PROTOCOL = "http";
+    private static final int PORT = 5000;
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String host, String endpoint, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(host, endpoint), params, responseHandler);
+    }
+
+    public static void post(String host, String endpoint, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(host, endpoint), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String host, String endpoint) {
+//        return PROTOCOL + "://" + host + Integer.toString(PORT) + "/" + endpoint;
+        return "http://www.reddit.com/.json";
+    }
+
+}
