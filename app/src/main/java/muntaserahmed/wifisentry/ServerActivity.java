9db83f3d5c36728fa807b14b7f9c3b0e5d14ebfb
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-11-08 13:13:38 -0500

    redesigned testing

--- app/src/main/java/muntaserahmed/wifisentry/ServerActivity.java
+++ app/src/main/java/muntaserahmed/wifisentry/ServerActivity.java
@@ -3,12 +3,25 @@ package muntaserahmed.wifisentry;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 public class ServerActivity extends Activity {
 
@@ -63,60 +76,74 @@ public class ServerActivity extends Activity {
         String ip = serverUrl.getText().toString();
         editor.putString("serverIp", ip);
         editor.commit();
-        Toast.makeText(getApplicationContext(), "Server Saved", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Server saved", Toast.LENGTH_SHORT).show();
     }
 
+    public void onTestBtnClicked(View v) throws JSONException {
 
-//    public void onSendBtnClicked(View v) {
-//        String baseUrl = serverUrl.getText().toString();
-//        if(v.getId() == R.id.save_button) {
-//            try {
-//                sendLightInstructions(baseUrl);
-//            } catch (JSONException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }
-//    }
-//
-//    public void sendLightInstructions(String baseUrl) throws JSONException {
-//
-//        JSONObject mainObj = new JSONObject();
-//        JSONArray lightArray = new JSONArray();
-//        JSONObject lightObject = new JSONObject();
-//
-//        lightObject.put("lightId", 1);
-//        lightObject.put("red", Integer.parseInt(redValText.getText().toString()));
-//        lightObject.put("green", Integer.parseInt(greenValText.getText().toString()));
-//        lightObject.put("blue", Integer.parseInt(blueValText.getText().toString()));
-//        lightObject.put("intensity", Double.parseDouble(intensityValText.getText().toString()));
-//
-//        lightArray.put(lightObject);
-//
-//        mainObj.put("lights", lightArray);
-//        mainObj.put("propagate", true);
-//
-//        try {
-//            StringEntity jsonEntity = new StringEntity(mainObj.toString());
-//            jsonEntity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-//
-//            // pass in values from server edittext
-//            RestClient.post(getApplicationContext(), baseUrl, "rpi", jsonEntity, "application/json", new JsonHttpResponseHandler() {
-//                @Override
-//                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                    Toast.makeText(getApplicationContext(), "JSONObject", Toast.LENGTH_SHORT).show();
-//                }
-//
-//                @Override
-//                public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-//                    Toast.makeText(getApplicationContext(), "JSONArray", Toast.LENGTH_SHORT).show();
-//                }
-//            });
-//
-//            Toast.makeText(getApplicationContext(), "Sent", Toast.LENGTH_SHORT).show();
-//
-//        } catch (UnsupportedEncodingException e) {
-//            Log.d("EXCEPTION:", "ENCODING");
-//        }
-//    }
+        if (!preferences.contains("serverIp")) {
+            Toast.makeText(getApplicationContext(), "Add a server first", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String serverAddress = preferences.getString("serverIp", null);
+
+        JSONObject mainObj = new JSONObject();
+        JSONArray lightArray = new JSONArray();
+        JSONObject lightObject = new JSONObject();
+
+        lightObject.put("lightId", 1);
+        lightObject.put("intensity", 0);
+
+        switch (v.getId()) {
+            case R.id.redButton:
+                lightObject.put("red", 255);
+                lightObject.put("green", 0);
+                lightObject.put("blue", 0);
+                break;
+            case R.id.greenButton:
+                lightObject.put("red", 0);
+                lightObject.put("green", 255);
+                lightObject.put("blue", 0);
+                break;
+            case R.id.blueButton:
+                lightObject.put("red", 0);
+                lightObject.put("green", 0);
+                lightObject.put("blue", 255);
+                break;
+            default:
+                lightObject.put("red", 255);
+                lightObject.put("green", 255);
+                lightObject.put("blue", 255);
+                break;
+        }
+
+        lightArray.put(lightObject);
+        mainObj.put("lights", lightArray);
+        mainObj.put("propagate", true);
+
+        try {
+            StringEntity jsonEntity = new StringEntity(mainObj.toString());
+            jsonEntity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+
+            RestClient.post(getApplicationContext(), serverAddress, "rpi", jsonEntity, "application/json", new JsonHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    Toast.makeText(getApplicationContext(), "JSONObject", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                    Toast.makeText(getApplicationContext(), "JSONArray", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            Toast.makeText(getApplicationContext(), "Sent", Toast.LENGTH_SHORT).show();
+
+        } catch (UnsupportedEncodingException e) {
+            Log.d("EXCEPTION:", "ENCODING");
+        }
+
+    }
 
 }
