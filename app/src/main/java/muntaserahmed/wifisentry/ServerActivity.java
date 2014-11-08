d1b6c6d310ce5ddf699224b75014576099a1bf09
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-11-07 22:03:08 -0500

    fixing serveractivity

--- app/src/main/java/muntaserahmed/wifisentry/ServerActivity.java
+++ app/src/main/java/muntaserahmed/wifisentry/ServerActivity.java
@@ -3,25 +3,12 @@ package muntaserahmed.wifisentry;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.loopj.android.http.JsonHttpResponseHandler;
-
-import org.apache.http.Header;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.protocol.HTTP;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
 
 public class ServerActivity extends Activity {
 
@@ -30,34 +17,27 @@ public class ServerActivity extends Activity {
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
 
-    EditText redValText;
-    EditText greenValText;
-    EditText blueValText;
-    EditText intensityValText;
-
     EditText serverUrl;
-    Button sendButton;
+    Button saveButton;
+
+    Button redButton;
+    Button greenButton;
+    Button blueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server);
 
-
         preferences = getSharedPreferences(SERVER_PREFERENCES, MODE_PRIVATE);
         editor = preferences.edit();
 
-
-        Toast.makeText(getApplicationContext(), preferences.getString("serverIp", "Please add a server"), Toast.LENGTH_SHORT).show();
-
-
         serverUrl = (EditText) findViewById(R.id.server_url);
-        sendButton = (Button) findViewById(R.id.save_button);
+        saveButton = (Button) findViewById(R.id.save_button);
 
-        redValText = (EditText) findViewById(R.id.red_edit);
-        greenValText = (EditText) findViewById(R.id.green_edit);
-        blueValText = (EditText) findViewById(R.id.blue_edit);
-        intensityValText = (EditText) findViewById(R.id.intensity_edit);
+        redButton = (Button) findViewById(R.id.redButton);
+        greenButton = (Button) findViewById(R.id.greenButton);
+        blueButton = (Button) findViewById(R.id.blueButton);
     }
 
     @Override
@@ -86,56 +66,57 @@ public class ServerActivity extends Activity {
         Toast.makeText(getApplicationContext(), "Server Saved", Toast.LENGTH_SHORT).show();
     }
 
-    public void onSendBtnClicked(View v) {
-        String baseUrl = serverUrl.getText().toString();
-        if(v.getId() == R.id.save_button) {
-            try {
-                sendLightInstructions(baseUrl);
-            } catch (JSONException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
 
-    public void sendLightInstructions(String baseUrl) throws JSONException {
-
-        JSONObject mainObj = new JSONObject();
-        JSONArray lightArray = new JSONArray();
-        JSONObject lightObject = new JSONObject();
-
-        lightObject.put("lightId", 1);
-        lightObject.put("red", Integer.parseInt(redValText.getText().toString()));
-        lightObject.put("green", Integer.parseInt(greenValText.getText().toString()));
-        lightObject.put("blue", Integer.parseInt(blueValText.getText().toString()));
-        lightObject.put("intensity", Double.parseDouble(intensityValText.getText().toString()));
-
-        lightArray.put(lightObject);
-
-        mainObj.put("lights", lightArray);
-        mainObj.put("propagate", true);
-
-        try {
-            StringEntity jsonEntity = new StringEntity(mainObj.toString());
-            jsonEntity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-
-            // pass in values from server edittext
-            RestClient.post(getApplicationContext(), baseUrl, "rpi", jsonEntity, "application/json", new JsonHttpResponseHandler() {
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                    Toast.makeText(getApplicationContext(), "JSONObject", Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                    Toast.makeText(getApplicationContext(), "JSONArray", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-            Toast.makeText(getApplicationContext(), "Sent", Toast.LENGTH_SHORT).show();
-
-        } catch (UnsupportedEncodingException e) {
-            Log.d("EXCEPTION:", "ENCODING");
-        }
-    }
+//    public void onSendBtnClicked(View v) {
+//        String baseUrl = serverUrl.getText().toString();
+//        if(v.getId() == R.id.save_button) {
+//            try {
+//                sendLightInstructions(baseUrl);
+//            } catch (JSONException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//    }
+//
+//    public void sendLightInstructions(String baseUrl) throws JSONException {
+//
+//        JSONObject mainObj = new JSONObject();
+//        JSONArray lightArray = new JSONArray();
+//        JSONObject lightObject = new JSONObject();
+//
+//        lightObject.put("lightId", 1);
+//        lightObject.put("red", Integer.parseInt(redValText.getText().toString()));
+//        lightObject.put("green", Integer.parseInt(greenValText.getText().toString()));
+//        lightObject.put("blue", Integer.parseInt(blueValText.getText().toString()));
+//        lightObject.put("intensity", Double.parseDouble(intensityValText.getText().toString()));
+//
+//        lightArray.put(lightObject);
+//
+//        mainObj.put("lights", lightArray);
+//        mainObj.put("propagate", true);
+//
+//        try {
+//            StringEntity jsonEntity = new StringEntity(mainObj.toString());
+//            jsonEntity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+//
+//            // pass in values from server edittext
+//            RestClient.post(getApplicationContext(), baseUrl, "rpi", jsonEntity, "application/json", new JsonHttpResponseHandler() {
+//                @Override
+//                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+//                    Toast.makeText(getApplicationContext(), "JSONObject", Toast.LENGTH_SHORT).show();
+//                }
+//
+//                @Override
+//                public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+//                    Toast.makeText(getApplicationContext(), "JSONArray", Toast.LENGTH_SHORT).show();
+//                }
+//            });
+//
+//            Toast.makeText(getApplicationContext(), "Sent", Toast.LENGTH_SHORT).show();
+//
+//        } catch (UnsupportedEncodingException e) {
+//            Log.d("EXCEPTION:", "ENCODING");
+//        }
+//    }
 
 }
