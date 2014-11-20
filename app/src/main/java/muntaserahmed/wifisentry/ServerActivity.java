33a7de71317265f6ae7a533bb2ef286b5843255e
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-11-19 23:06:13 -0500

    preparing for cardboard

--- app/src/main/java/muntaserahmed/wifisentry/ServerActivity.java
+++ app/src/main/java/muntaserahmed/wifisentry/ServerActivity.java
@@ -66,9 +66,9 @@ public class ServerActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -145,5 +145,6 @@ public class ServerActivity extends Activity {
         }
 
     }
+
 
 }
